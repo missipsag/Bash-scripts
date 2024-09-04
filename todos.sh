71656b9bf19dfa4200f8c1b2cc8cb48bebcccbee
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2024-09-04 20:51:05 +0200

    make some necessary changes

--- todos.sh
+++ todos.sh
@@ -1,42 +1,13 @@
 #!/bin/bash
 
+#This a script that does exactly what a todo app does but on the terminal
+
 TODO_FILE="$HOME/.todo-list"
 
 if [ ! -f "$TODO_File" ];
 then 
     touch "$TODO_FILE"
 fi 
-
-#todos array
-todos=()
-
-#todos to delete array
-todos_to_delete=()
-
-j=0
-
-if [ $1 == "add" ]  #check if the option is add
-then 
-    #shifting so that we don't include the option in our array
-    shift
-    for i in {2..$#}
-    do
-        ${todos[$j]}="$1"
-        $((++j))
-        shift  
-    done
-elif [ $1 == "remove" ]  #check if the option is remove
-then
-    #shifting so that we don't include the option in our array
-    shift
-    for i in {2..$#}
-    do
-        ${todos_to_delete[$j]}="$1"
-        $((++j))
-        shift  
-    done
-fi
-
 
 
 function help () {
@@ -45,29 +16,27 @@ function help () {
     echo ""
     echo "verbs :"
     echo ""
-    echo "add, -a : add a task to the todo list, it accepts String"
-    echo "remove, rm, -r  : removes a task from the todo list, accepts either Strings or todos indecies"
-    echo "show, sh, -s : shows the current tasks "
+    echo "add : add a task to the todo list, it accepts String"
+    echo "remove, rm : removes a task from the todo list, accepts either Strings or todos indecies"
+    echo "show : shows the current tasks "
     echo " "
 }
 
 
 function show () {
-    for todo in ${todos[@]}
-    do 
-        echo "$todo"
-    done
+    echo "Current todos : "
+    cat $TODO_FILE
 }
 
 function add () { 
 
-
+    echo " **********INSIDE THE ADD FUNCTION ****************"
     #here we use a for loop to iterate through the todos array and add them to the list 
     #${#todos[@]} is the length of the todos array
-
-    for index in ${#todos[@]}
+    local todos="$1"
+    for i in "${#todos[@]}";
     do 
-        echo " ${todos[$index]}" >> TODO_FILE
+        echo " ${todos[$i]}" 
     done    
     
 }
@@ -75,27 +44,50 @@ function add () {
 
 function remove () {
 
-    #here we ask the user to enter a task or an array of todos
-    read -p -a "please enter the task or task number to delete : " todos_to_delete
+    
     if [ ${#todos_to_delete[@]} -eq 0 ]; 
     then 
         echo "please pass in an argument, try again"
     else 
-       for index in {0..${#todos[@]}}
+       for i in {0..${#todos[@]}}
         do 
-            sed "${task_to_delete[$index]}d"
+            sed "${task_to_delete[i]}d" TODO_FILE
         done 
     fi
 }
 
 
 
-case $1 in 
-    --help | -h) help 
+case "$1" in 
+     help) help 
     ;;
-    add | -a) add "$2"
+    add ) 
+        #todos array
+        declare -a todos
+
+        j=0
+        shift #shifting so that we don't include the option in our array
+        while [ $# -gt 0 ]
+        do
+            ${todos[$j]}="$1"
+            ((j++))
+            shift  
+        done
+        add "todos"
     ;;
-    remove | rm | -r) remove "$2"
+    remove | rm ) 
+        #todos to delete array
+        declare -a todos_to_delete
+
+        j=0
+        shift #shifting so that we don't include the option in our array
+        while [ $# -gt 0 ]
+        do
+            todos_to_delete[$j]="$1"
+            ((j++))
+            shift  
+        done
+        remove "todos_to_delete"
     ;;
-    show | -s | sh) show  
+    show ) show  
 esac 
