e5b23b2274fadb54993b3260a724fc4c4813a6b1
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2024-10-09 15:12:53 +0200

    added description

--- connect_to_wifi.sh
+++ connect_to_wifi.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+#this is a bash script I use to connect to my wifi. Because currently I use debian 11 (bullseye),
+#I am encountring some difficulties connecting to wifi throgh networkManager. I thinck it's due to 
+#some incompatibility between my my network card and Debian Os.
+#Luckily, after doing some reserch I found out about some terminal commands to connect to wifi "manually"
+# meaning not through natworkManager
+
+#Note : this is should be done after initiating a password handshake (connecting to wifi for the first 
+# time, and passing the password) with the command wpa-passphrase.
+
 #deactivate all enabled  network interfaces 
 sudo /sbin/ifdown -a --read-environment 
-
 
 sudo rm /run/wpa_supplicant/wlo1
 
 #join a protected wireless network and run it in a deamon 
 sudo wpa_supplicant -B -i wlo1 -c /etc/wpa_supplicant/wpa_supplicant-wlo1.conf
-
 
 #reactivate all disabled networking interfaces
 sudo /sbin/ifup -a --read-environment 
