a83443c1fd66a96c3c6b40f70d5d3e24e9f8015f
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2024-10-09 15:20:33 +0200

    add a description

--- todos.sh
+++ todos.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 #This a script that does exactly what a todo app does but on the terminal
+#I made it so that I can check my todo list directly on my terminal whithout having 
+# to open an external program and getting distracted and wasting what could be valuable 
+# focus time
 
-TODO_FILE="$HOME/.todo-list"
+TODO_FILE="$HOME/.todo-list" #declare a file where to put the todos
 
 if [ ! -f "$TODO_File" ]; #check if the todo-list file is already created ( if its the first time using the script)
 then 
     touch "$TODO_FILE"
 fi 
-
-
-
-
-
-
 
 case "$1" in 
      help) 
@@ -23,7 +20,7 @@ case "$1" in
         echo "verbs :"
         echo ""
         echo "add : add a task to the todo list, it accepts String"
-        echo "remove, rm : removes a task from the todo list, accepts a number"
+        echo "clear : clears/removes a task from the todo list, accepts a number"
         echo "show : shows the current tasks "
         echo " "
     ;;
@@ -32,7 +29,7 @@ case "$1" in
        echo ""
         
     ;;
-    remove | rm )
+    clear || cl)
         if [ ! $2 ]; #check if the line number was provided by the user
         then 
             echo "please enter the line number of the task to delete"
